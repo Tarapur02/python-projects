452a313fef9c7898fb495fcc2b7ea7c267de4c70
Viresh Tarapur <tarapurviresh@gmail.com>
2025-09-18 18:31:49 +0530

    main.py

--- To-Do List/main.py
+++ To-Do List/main.py
@@ -1,48 +1,52 @@
-def task():
-    print("Welcome To the To-Do List..")
-    task_list = []
-    total_task = int(input("How many tasks do you want to add initially? : "))
+def To_do():
+    print("Welcome to To-Do List ")
 
-    for i in range(1, total_task + 1):
-        tasks = input(f"Enter task {i}: ")
-        task_list.append(tasks)
-    print(f"The tasks are: {task_list}")
+    task = []
+    task_list = int(input("Enter how many tasks you want to add in the task list: "))
+    for i in range(1, task_list + 1):
+        task_add = input(f" Enter Task {i}: ")
+        task.append(task_add)
+    print(f"Today's tasks are: {task}")
 
     while True:
-        operation = input("\nEnter \n 1 = Add \n 2 = Update \n 3 = Delete \n 4 = View \n 5 = Exit\nChoice: ")
+        operation = int(input("\nEnter \n 1 = Add \n 2 = Update \n 3 = Delete \n 4 = View \n 5 = Exit\nChoice: "))
+        
+        if operation == 1:
+            new = input("Enter the new task: ")
+            task.append(new)
+            print(f"Task added! Current tasks: {task}")
 
-        if operation == "1":
-            new_task = input("Enter the new task to add: ")
-            task_list.append(new_task)
-            print("New task added")
-            print(f"{task_list}")
-
-        elif operation == "2":
+        elif operation == 2:
             old_task = input("Enter the task you want to update: ")
-            if old_task in task_list:
+            if old_task in task:
                 new_task = input("Enter the new task: ")
-                ind = task_list.index(old_task)
-                task_list[ind] = new_task
-                print(f"Updated To-Do List: {task_list}")
+                ind = task.index(old_task)
+                task[ind] = new_task
+                print(f"Task updated! Current tasks: {task}")
             else:
                 print("Task not found")
 
-        elif operation == "3":
-            del_task = input("Enter the task you want to delete: ")
-            if del_task in task_list:
-                task_list.remove(del_task)
-                print(f" Updated To-Do List: {task_list}")
+        elif operation == 3:
+            delete = input("Enter the task you want to remove: ")
+            if delete in task:
+                task.remove(delete)
+                print(f"Task removed! Current tasks: {task}")
             else:
                 print("Task not found")
 
-        elif operation == "4":
-            print(f" Your To-Do List: {task_list}")
+        elif operation == 4:
+            print("\nCurrent To-Do List:")
+            if task:
+                for i, t in enumerate(task, start=1):
+                    print(f"{i}. {t}")
+            else:
+                print("No tasks available.")
 
-        elif operation == "5":
-            print(" Completed all tasks. Exiting...")
+        elif operation == 5:
+            print("Exiting To-Do List. Goodbye!")
             break
 
         else:
-            print("Invalid Input")
+            print("Invalid Input....")
 
-task()
+To_do()
