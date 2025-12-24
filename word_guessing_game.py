90f711bbc46f898950c8f56558cab0d4ac3b0eea
Viresh Tarapur <tarapurviresh@gmail.com>
2025-12-24 23:45:09 +0530

    Update word_guessing_game.py

--- word_guessing_game.py
+++ word_guessing_game.py
@@ -14,45 +14,66 @@ Hard_words = [
     "apple", "chair", "bread", "plant", "stone", "cloud", "water", "earth", "river", "smile",
     "green", "light", "sound", "train", "house", "music", "phone", "glass", "dream", "night"
 ]
+def normal(level_1):
+    while True:
+        user_input=input("Enter The Word: ")
+        if user_input == level_1:
+            print("Your Guess is Currect..")
+            thank()
+        elif user_input!= level_1:
+            for i in range(len(level_1)):
+                for j in range(len(user_input)):
+                    if level_1[i] == user_input[j]:
+                        print(f"The {user_input[i]} is Currect Position..  ")
 
-print(random.choice(Medium_words))
+def medium(level_2):
+    while True:
+        user_input=input("Enter The Word: ")
+        if user_input == level_2:
+            print("Your Guess is Currect..")
+            thank()
+        elif user_input!= level_2:
+            for i in range(len(level_2)):
+                for j in range(len(user_input)):
+                    if level_2[i] == user_input[j]:
+                        print(f"The {user_input[i]} is Currect Position..  ")
+
+def hard(level_3):
+    while True:
+        user_input=input("Enter The Word: ")
+        if user_input == level_3:
+            print("Your Guess is Currect..")
+            thank()
+        elif user_input!= level_3:
+            for i in range(len(level_3)):
+                for j in range(len(user_input)):
+                    if level_3[i] == user_input[j]:
+                        print(f"The {user_input[i]} is Currect Position..  ")
 
 def thank():
     while True:
         play=input("You Want Play More (Y/N): ") .lower()
         if play== "y":
-            pass
+            main() 
         else:
-            print("Thank you Have a Goof Day....")
+            print("Thank you Have a Good Day....")
             break
 
 
-while True:
-    user_input_leval= input("Please Enter What difficulty Leval You what ex:(Normal,Medium,Hard) : ").lower()
-    count=0
-    if user_input_leval == "normal":
-        leval_1=random.choice(Normal_words)
-        print(leval_1)
-        user_input=input("Enetr The Word..: ")
-        if user_input == leval_1:
-            print("Your Guess is Curect..")
-            thank()
-        for i in range(len(leval_1)):
-            print(leval_1[i])
-        print(leval_1)
+def main():
+    user_input_level = input("Please Enter What difficulty Leval You what ex:(Normal,Medium,Hard) : ").lower()
 
-        break
-    elif user_input_leval == "medium":
-        leval_2=random.choice(Medium_words)
-        print(leval_2)
-        break
-    elif user_input_leval == "hard":
-        leval_3=random.choice(Hard_words)
-        print(leval_3)
-        break
+    if user_input_level == "normal":
+        level_1=random.choice(Normal_words)
+        normal(level_1)
+    elif user_input_level == "medium":
+        level_2=random.choice(Medium_words)
+        medium(level_2)
+    elif user_input_level == "hard":
+        level_3=random.choice(Hard_words)
+        hard(level_3)
     else:
-        print("Please Enter valied Input : ")
-
-
-
+        main()
     
+
+main()
